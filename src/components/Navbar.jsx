ce1edb859ec0915ffb7fe7e5cdcd4a4d539bd2d6
Tristan Golding <Tristan@geblertooth.co.uk>
2025-07-19 12:41:34 +0100

    feat(navbar): added react-icons for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import './Navbar.css';
+import { MdDashboard, MdHome, MdSettings } from "react-icons/md";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">Dashboard</div>
+      <div className="navbar-logo"><MdDashboard /></div>
       <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/settings">Settings</Link></li>
+        <li><Link to="/"><MdHome /></Link></li>
+        <li><Link to="/settings"><MdSettings /></Link></li>
       </ul>
     </nav>
   );
